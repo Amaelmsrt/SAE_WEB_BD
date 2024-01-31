8271fa67f8a01cef9f000982e7906032082c3a31
seb_grtd <sebastien.gratade@etu.univ-orleans.fr>
2024-01-31 19:36:15 +0100

    ajout possibilité de changer d'onglet sur l'accueil avec transition qui s'adapte en fonction de l'onglet d'où on vient et de celui où on va (on va de droite à gauche ou de gauche à droite)

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -6,9 +6,32 @@ const goToPlaylists = document.querySelector("#goToPlaylists")
 
 const songcards = document.querySelectorAll(".song-card");
 
+const sectionAccueil = document.querySelector("#SectionAccueil")
+const sectionRechercher = document.querySelector("#SectionRecherche")
+const sectionPlaylists = document.querySelector("#SectionPlaylists") 
+
+function clearActiveSections(){
+    if (sectionAccueil.classList != null)
+        sectionAccueil.classList.remove("active-section")
+    if (sectionRechercher.classList != null)
+    sectionRechercher.classList.remove("active-section")
+    if (sectionPlaylists.classList != null)
+        sectionPlaylists.classList.remove("active-section")
+}
+
 // Fonction pour changer de formulaire
 function changeCurrentMenu(e,index) {
     e?.preventDefault();
+    const curSection = document.querySelector(".active-section")
+    let curIndex = -1
+    if (e){
+        const curSectionName = curSection.id.split("Section")[1]
+        
+        curIndex = curSectionName == "Accueil" ? 1 : curSectionName == "Recherche" ? 2 : curSectionName == "Playlists" ? 3 : -1
+        if (e.target.id.split("goTo")[1] == curSectionName){
+            return;
+        }
+    }
     // Si le formulaire d’inscription est visible, le cacher et montrer le formulaire de connexion
     // padding 1rem gap 0.65rem width des elements 11.25
     // 1 : 1rem
@@ -24,16 +47,33 @@ function changeCurrentMenu(e,index) {
             gsap.to(goToAccueilBtn, { color: "#0E100F", duration: 0.6,ease: "power4.out" });
             gsap.to(goToRechercheBtn, { color: "#FEFCE1", duration: 0.6,ease: "power4.out" });
             gsap.to(goToPlaylists, { color: "#FEFCE1", duration: 0.6,ease: "power4.out" });
+            
+            gsap.fromTo(sectionAccueil, {opacity: 0, x: index >= curIndex ? "-100vw" : "100vw", zIndex: 1}, {opacity:1, x:0, duration:0.6, ease:"power4.out"})
+            gsap.to(curSection, {opacity:0, x: index >= curIndex ? "100vw" : "-100vw", duration:0.6, ease: "power4.out"})
+            clearActiveSections()
+            sectionAccueil.classList.add("active-section")
             break;
         case 1:
             gsap.to(goToAccueilBtn, { color: "#FEFCE1", duration: 0.6,ease: "power4.out" });
             gsap.to(goToRechercheBtn, { color: "#0E100F", duration: 0.6,ease: "power4.out" });
             gsap.to(goToPlaylists, { color: "#FEFCE1", duration: 0.6,ease: "power4.out" });
+
+            gsap.fromTo(sectionRechercher, {opacity: 0, x: index >= curIndex ? "-100vw" : "100vw"}, {opacity:1, x:0, duration:0.6, ease:"power4.out"})
+            gsap.to(curSection, {opacity:0, x: index >= curIndex ? "100vw" : "-100vw", duration:0.6, ease: "power4.out"})
+
+            clearActiveSections()
+            sectionRechercher.classList.add("active-section")
             break;
         case 2:
             gsap.to(goToAccueilBtn, { color: "#FEFCE1", duration: 0.6,ease: "power4.out" });
             gsap.to(goToRechercheBtn, { color: "#FEFCE1", duration: 0.6,ease: "power4.out" });
             gsap.to(goToPlaylists, { color: "#0E100F", duration: 0.6,ease: "power4.out" });
+            
+            gsap.fromTo(sectionPlaylists, {opacity: 0, x: index >= curIndex ? "-100vw" : "100vw"}, {opacity:1, x:0, duration:0.6, ease:"power4.out"})
+            gsap.to(curSection, {opacity:0, x:index>=curIndex ? "100vw" : "-100vw", duration:0.6, ease: "power4.out"})
+
+            clearActiveSections()
+            sectionPlaylists.classList.add("active-section")
             break;
     }
 }
