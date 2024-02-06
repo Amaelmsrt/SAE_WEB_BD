057976ca55d3839d2f190465c4593b46556c03ef
Amael Maserati <amael.maserati@etu.univ-orleans.fr>
2024-02-06 13:31:55 +0100

    l'animation de la navbar fonctionne

--- src/Assets/Js/mainscript_admin.js
+++ src/Assets/Js/mainscript_admin.js
@@ -7,8 +7,42 @@ const goToUtilisateurs = document.querySelector("#goToUtilisateurs");
 const goToPlaylists = document.querySelector("#goToPlaylists");
 const goToAlbums = document.querySelector("#goToAlbums");
 
+const sectionMenuPrincipal = document.querySelector("#pagePrincipale");
+const sectionMenuArtistes = document.querySelector("#pageArtistes");
+const sectionMenuSons = document.querySelector("#pageSons");
+const sectionMenuGenres = document.querySelector("#pageGenres");
+const sectionMenuUtilisateurs = document.querySelector("#pageUtilisateurs");
+const sectionMenuPlaylists = document.querySelector("#pagePlaylists");
+const sectionMenuAlbums = document.querySelector("#pageAlbums");
+
 function changeCurrentMenu(e, index) {
     e?.preventDefault();
+
+    function clearActiveSections() {
+        if (sectionMenuPrincipal.classList != null)
+            sectionMenuPrincipal.classList.remove("active-section");
+        if (sectionMenuArtistes.classList != null)
+            sectionMenuArtistes.classList.remove("active-section");
+        if (sectionMenuSons.classList != null)
+            sectionMenuSons.classList.remove("active-section");
+        if (sectionMenuGenres.classList != null)
+            sectionMenuGenres.classList.remove("active-section");
+        if (sectionMenuUtilisateurs.classList != null)
+            sectionMenuUtilisateurs.classList.remove("active-section");
+        if (sectionMenuPlaylists.classList != null)
+            sectionMenuPlaylists.classList.remove("active-section");
+        if (sectionMenuAlbums.classList != null)
+            sectionMenuAlbums.classList.remove("active-section");
+    }
+
+    const curSection = document.querySelector(".active-section");
+    let curIndex = -1;
+    if (e) {
+        const curSectionName = curSection.id.split("page")[1];
+
+        curIndex = curSectionName == "Principale" ? 0 : curSectionName == "Artistes" ? 1 : curSectionName == "Sons" ? 2 : curSectionName == "Genres" ? 3 : curSectionName == "Utilisateurs" ? 4 : curSectionName == "Playlists" ? 5 : 6;
+        if (e.target.id.split("goTo")[1] == curSectionName) return;
+    }
 
     const menuItemHeight = 3.5;
 
@@ -35,6 +69,11 @@ function changeCurrentMenu(e, index) {
             gsap.to(goToUtilisateurs, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
             gsap.to(goToPlaylists, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
             gsap.to(goToAlbums, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
+
+            gsap.fromTo(sectionMenuPrincipal, {opacity: 0, y: index < curIndex ? "100vw" : "-100vw"}, {opacity: 1, y: 0, duration: 0.6, zIndex: 1, ease:"power4.out"});
+            gsap.to(curSection, {opacity:0, y: index < curIndex ? "-100vw" : "100vw", duration:0.6, zIndex: -1, ease: "power4.out"});
+            clearActiveSections();
+            sectionMenuPrincipal.classList.add("active-section");
             break;
 
         case 1:
@@ -44,7 +83,13 @@ function changeCurrentMenu(e, index) {
             gsap.to(goToGenres, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
             gsap.to(goToUtilisateurs, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
             gsap.to(goToPlaylists, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
-            gsap.to(goToAlbums, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});           
+            gsap.to(goToAlbums, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
+
+            gsap.fromTo(sectionMenuArtistes, {opacity: 0, y: index < curIndex ? "100vw" : "-100vw"}, {opacity: 1, y: 0, duration: 0.6, zIndex: 1, ease:"power4.out"});
+            gsap.to(curSection, {opacity:0, y: index < curIndex ? "-100vw" : "100vw", duration:0.6, zIndex: -1, ease: "power4.out"});
+
+            clearActiveSections();
+            sectionMenuArtistes.classList.add("active-section");
             break;
 
         case 2:
@@ -55,6 +100,12 @@ function changeCurrentMenu(e, index) {
             gsap.to(goToUtilisateurs, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
             gsap.to(goToPlaylists, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
             gsap.to(goToAlbums, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
+
+            gsap.fromTo(sectionMenuSons, {opacity: 0, y: index < curIndex ? "100vw" : "-100vw"}, {opacity: 1, y: 0, duration: 0.6, zIndex: 1, ease:"power4.out"});
+            gsap.to(curSection, {opacity:0, y: index < curIndex ? "-100vw" : "100vw", duration:0.6, zIndex: -1, ease: "power4.out"});
+
+            clearActiveSections();
+            sectionMenuSons.classList.add("active-section");
             break;
 
         case 3:
@@ -65,6 +116,12 @@ function changeCurrentMenu(e, index) {
             gsap.to(goToUtilisateurs, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
             gsap.to(goToPlaylists, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
             gsap.to(goToAlbums, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
+
+            gsap.fromTo(sectionMenuGenres, {opacity: 0, y: index < curIndex ? "100vw" : "-100vw"}, {opacity: 1, y: 0, duration: 0.6, zIndex: 1, ease:"power4.out"});
+            gsap.to(curSection, {opacity:0, y: index < curIndex ? "-100vw" : "100vw", duration:0.6, zIndex: -1, ease: "power4.out"});
+
+            clearActiveSections();
+            sectionMenuGenres.classList.add("active-section");
             break;
 
         case 4:
@@ -74,7 +131,13 @@ function changeCurrentMenu(e, index) {
             gsap.to(goToGenres, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
             gsap.to(goToUtilisateurs, {color: "#0E100F", duration: 0.6, ease:"power4.out"});
             gsap.to(goToPlaylists, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
-            gsap.to(goToAlbums, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});   
+            gsap.to(goToAlbums, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
+
+            gsap.fromTo(sectionMenuUtilisateurs, {opacity: 0, y: index < curIndex ? "100vw" : "-100vw"}, {opacity: 1, y: 0, duration: 0.6, zIndex: 1, ease:"power4.out"});
+            gsap.to(curSection, {opacity:0, y: index < curIndex ? "-100vw" : "100vw", duration:0.6, zIndex: -1, ease: "power4.out"});
+
+            clearActiveSections();
+            sectionMenuUtilisateurs.classList.add("active-section");
             break;
         case 5:
             gsap.to(goToMenuPrincipal, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
@@ -84,6 +147,12 @@ function changeCurrentMenu(e, index) {
             gsap.to(goToUtilisateurs, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
             gsap.to(goToPlaylists, {color: "#0E100F", duration: 0.6, ease:"power4.out"});
             gsap.to(goToAlbums, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
+
+            gsap.fromTo(sectionMenuPlaylists, {opacity: 0, y: index < curIndex ? "100vw" : "-100vw"}, {opacity: 1, y: 0, duration: 0.6, zIndex: 1, ease:"power4.out"});
+            gsap.to(curSection, {opacity:0, y: index < curIndex ? "-100vw" : "100vw", duration:0.6, zIndex: -1, ease: "power4.out"});
+
+            clearActiveSections();
+            sectionMenuPlaylists.classList.add("active-section");
             break;
         case 6:
             gsap.to(goToMenuPrincipal, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
@@ -93,6 +162,12 @@ function changeCurrentMenu(e, index) {
             gsap.to(goToUtilisateurs, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
             gsap.to(goToPlaylists, {color: "#FEFCE1", duration: 0.6, ease:"power4.out"});
             gsap.to(goToAlbums, {color: "#0E100F", duration: 0.6, ease:"power4.out"});
+
+            gsap.fromTo(sectionMenuAlbums, {opacity: 0, y: index < curIndex ? "100vw" : "-100vw"}, {opacity: 1, y: 0, duration: 0.6, zIndex: 1, ease:"power4.out"});
+            gsap.to(curSection, {opacity:0, y: index < curIndex ? "-100vw" : "100vw", duration:0.6, zIndex: -1, ease: "power4.out"});
+
+            clearActiveSections();
+            sectionMenuAlbums.classList.add("active-section");
             break;
     }
 }
